e7d69a575bffd8ed61ee9e964ae7f96115de3ec4
lsf <xiaolian960715@foxmail.com>
2020-12-14 17:46:52 +0800

    希尔排序

---
+++ Arithmetic/ShellSort/ShellSort.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+#define sSUCCESS	0
+#define sERROR  -1
+
+template<class T>
+int PrintArray(T *aArray, int aCount)
+{
+	cout<<"aArray[]=";
+	for (int i = 0; i < aCount; i++)
+	{
+		cout<<aArray[i]<<"   ";
+	}
+	cout<<endl;
+	return sSUCCESS;
+}
+
+int ShellSort(T *aArray, int aCount)
+{
+	if(0 == aCount || NULL == aArray)
+		return sERROR;
+	
+	for(int i = aCount/2; i > 0; i/=2)
+	{
+		int j = i;
+		while(j - i >= 0 && aArray[j - i] > aArray[j])
+		{
+			aArray[j - i] = aArray[j - i] + aArray[j];
+			aArray[j]     = aArray[j - i] - aArray[j];
+			aArray[j - i] = aArray[j - i] - aArray[j];
+			j = j - i;
+		}
+	}
+	return sSUCCESS;
+}
+
+int main(int argc, char ** argv)
+{
+	int iArray[] = { 49,38,65,97,76,13,27,49,10 };
+	int iSize = sizeof(iArray) / sizeof(int);
+	
+	cout <<"排序前："<<endl;
+	PrintArray<int>(iArray, iSize);
+	
+	QuickSort<int>(iArray, 0, iSize - 1);
+	
+	cout <<"排序后："<<endl;
+	PrintArray<int>(iArray, iSize);
+	
+	double dArray[] = { 12.3,90.0,123.44,22.5,99.4,34.6,23.3,78.5,20.0 };
+	int dSize = sizeof(dArray) / sizeof(double);
+	
+	cout <<"排序前："<<endl;
+	PrintArray<double>(dArray, dSize);
+	
+	QuickSort<double>(dArray, 0, dSize - 1);
+	
+	cout <<"排序后："<<endl;
+	PrintArray<double>(dArray, dSize);
+	
+	return sSUCCESS;
+};
